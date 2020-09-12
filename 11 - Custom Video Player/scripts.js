63cc980da5f53cd2f20c47f1b524e261735e7aa4
zerokira123 <thegetbackerzero@gmail.com>
2020-09-12 23:47:17 +0530

    finished

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -33,8 +33,14 @@ function handleRangeUpdate() {
 function handleProgress() {
     const percent = (video.currentTime/video.duration) * 100;
     progressBar.style.flexBasis = `${percent}%`;
+    
 }
 
+function scrub(e){
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+    console.log(e); 
+}
 // event listeners
 video.addEventListener('click',togglePlay);
 video.addEventListener('play',updateButton);
@@ -44,6 +50,14 @@ skipButtons.forEach(button => button.addEventListener("click",skip));
 ranges.forEach(range => range.addEventListener("change",handleRangeUpdate));
 ranges.forEach(range => range.addEventListener("mousemove",handleRangeUpdate));
 video.addEventListener('timeupdate',handleProgress);
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+
+
 
 
 
