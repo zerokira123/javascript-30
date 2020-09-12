d02ff5bf222b8c2a2b87cf2c46989d06d12faf76
zerokira123 <thegetbackerzero@gmail.com>
2020-09-12 19:59:33 +0530

    started again

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,5 +1,6 @@
 const player = document.querySelector('.player');
-const video = player.querySelector(".progresss");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
 const progressbar = player.querySelector(".progress_filled");
 const toggle = player.querySelector(".toggle");
 const skipButtons = player.querySelectorAll("[data-skip]");
@@ -9,3 +10,5 @@ function togglePlay() {
     const method = video.paused ? 'play' : 'pause';
     video[method]();
 }
+
+video.addEventListener('click',togglePlay);
