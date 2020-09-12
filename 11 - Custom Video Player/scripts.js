4682c17d47c4d637ba10427d4923a48b6abc2ea1
zerokira123 <thegetbackerzero@gmail.com>
2020-09-12 11:38:33 +0530

    added toggleplay function

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -4,3 +4,8 @@ const progressbar = player.querySelector(".progress_filled");
 const toggle = player.querySelector(".toggle");
 const skipButtons = player.querySelectorAll("[data-skip]");
 const ranges = player.querySelectorAll(".player__slider");
+
+function togglePlay() {
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+}
