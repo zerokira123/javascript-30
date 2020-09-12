b1ebcf640dd3caeecaea84e3c28f804e235da61c
zerokira123 <thegetbackerzero@gmail.com>
2020-09-12 11:37:17 +0530

    Update scripts.js

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,6 @@
+const player = document.querySelector('.player');
+const video = player.querySelector(".progresss");
+const progressbar = player.querySelector(".progress_filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
